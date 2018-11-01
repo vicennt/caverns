a87cb097b9c231313b962eb151a56680118373f6
Vicent Perez <vicennt@gmail.com>
2018-11-01 16:25:04 +0000

    Second commit (Testing EGit Eclipse)

--- src/caverns/logic/MainClass.java
+++ src/caverns/logic/MainClass.java
@@ -1,8 +1,9 @@
 package caverns.logic;
 
+
 public class MainClass {
-	
 	public static void main(String[] args) {
-		System.out.println("First commit!");
+		System.out.println("------- Coursework Artificial Intelligence -------");
+		System.out.println("# Map information");		
 	}
 }
