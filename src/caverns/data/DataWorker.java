f774e6d31015978b27a633e010a9fa2de66bf2b5
Vicent Perez <vicennt@gmail.com>
2018-11-01 16:40:23 +0000

    Function "readFile" reads a CSV file (separated with ",") and save each word into a String array.

--- src/caverns/data/DataWorker.java
+++ src/caverns/data/DataWorker.java
@@ -1,5 +1,28 @@
 package caverns.data;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DataWorker {
+	
+	public String[] readFile(String fileNamePath) {   
+        File file = new File(fileNamePath);
+        Scanner inputStream;
+        String[] data = null;
+        try{
+            inputStream = new Scanner(file);
+            String aux = "";
+            while(inputStream.hasNext()){
+                aux += inputStream.next();
+            }
+            data = aux.split(",");
+            inputStream.close();
+            return data;
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
 
 }
