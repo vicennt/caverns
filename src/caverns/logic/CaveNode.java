1fc5a589c5d48b1310f7b7215dfb68a236259fda
Vicent Perez <vicennt@gmail.com>
2018-11-11 23:19:43 +0000

    Adding gScore attribute and compareTo, toString functions

--- src/caverns/logic/CaveNode.java
+++ src/caverns/logic/CaveNode.java
@@ -2,12 +2,20 @@ package caverns.logic;
 
 import java.util.ArrayList;
 
-public class CaveNode {
+public class CaveNode implements Comparable<CaveNode> {
 	
 	private int numCave;
 	private int coordenateX;
 	private int coordenateY;
+	private double gScore;
 	private ArrayList<CaveNode> nextCaves;
+	
+	public CaveNode(int coordenateX, int coordenateY, int numCave) {
+		this.coordenateX = coordenateX;
+		this.coordenateY = coordenateY;
+		this.numCave = numCave;
+		nextCaves = new ArrayList<CaveNode>();
+	}
 	
 	public int getNumCave() {
 		return numCave;
@@ -27,6 +35,13 @@ public class CaveNode {
 	public void setCoordenateY(int coordenateY) {
 		this.coordenateY = coordenateY;
 	}
+	
+	public double getgScore() {
+		return gScore;
+	}
+	public void setgScore(double gScore) {
+		this.gScore = gScore;
+	}
 
 	public void addCave(CaveNode c) {
 		nextCaves.add(c);
@@ -45,5 +60,27 @@ public class CaveNode {
 		return Math.sqrt(Math.pow((n.coordenateX - this.coordenateX),2) + 
 				Math.pow((n.coordenateY - this.coordenateY),2));
 	}
+	
+	@Override
+	public int compareTo(CaveNode o) {
+		if(this.gScore == o.gScore) 
+			return 0;
+		else if(this.gScore < o.gScore)
+			return -1;
+		else 
+			return 1;
+	}
+	@Override
+	public String toString() {
+		String myNeigbours = "";
+		for(int i = 0; i < nextCaves.size(); i++) {
+			myNeigbours += "[Neigbour " + nextCaves.get(i).getNumCave()+ "]";
+		}
+		return "[ID: " +this.numCave + "] [gScore: " + this.gScore + " ] " + myNeigbours;
+	}
+	
+	
+	
+	
 	
 }
