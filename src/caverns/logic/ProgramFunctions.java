c19eea5093fc0935a4f71fd423efb8b5d72498ba
Vicent Perez <vicennt@gmail.com>
2018-11-12 01:23:26 +0000

    [+] Adding again the euclidean and coordenates functions

--- src/caverns/logic/ProgramFunctions.java
+++ src/caverns/logic/ProgramFunctions.java
@@ -19,6 +19,18 @@ public class ProgramFunctions {
 		return Integer.parseInt(data[0]);
 	}
 	
+	public int[][] getCoordenateMatrix() {
+		int[][] coordenateMatrix = new int[numCaverns][2];	
+		int startPosition = 1;
+		for(int i = 0; i < numCaverns; i++) {
+			for (int j = 0; j < 2; j++) {
+				coordenateMatrix[i][j] = Integer.parseInt(data[startPosition]);
+				startPosition++;
+			}
+		}
+		return coordenateMatrix;
+	}
+	
 	public int[][] getIncidenceMatrix() {
 		int[][] incidenceMatrix = new int[numCaverns][numCaverns];	
 		int startPosition = (numCaverns * 2) + 1;
@@ -29,6 +41,28 @@ public class ProgramFunctions {
 			}
 		}	
 		return incidenceMatrix;
+	}
+	
+	public double[][] getEuclideanMatrix(int[][] incidenceMatrix, int[][] coordenateMatrix){
+		double[][] euclideanMatrix = new double[numCaverns][numCaverns];	
+		for(int i = 0; i < numCaverns; i++) {
+			for(int j = 0; j < numCaverns; j++) {
+				if(incidenceMatrix[i][j] == 1) { // Edge between node i and node j
+					int x1 = coordenateMatrix[i][0];
+					int x2 = coordenateMatrix[j][0];
+					int y1 = coordenateMatrix[i][1];
+					int y2 = coordenateMatrix[j][1];
+					double weighEdge = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+					euclideanMatrix[i][j] = round(weighEdge, 2);
+				}
+			}
+		}
+		return euclideanMatrix;	
+	}
+	
+	private static double round (double value, int precision) {
+	    int scale = (int) Math.pow(10, precision); 
+	    return (double) Math.round(value * scale) / scale;
 	}
 	
 	public ArrayList<CaveNode> getCaves() {
